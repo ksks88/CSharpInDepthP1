821436da2cd7b7b7b12fbd805d9e3367becb1cf9
Salih Hajlakovic <salih.hajlakovic@gmail.com>
2017-06-08 01:25:59 +0200

    Add project files.

---
+++ Product.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace CSharpInDepthP1
+{
+    public class Product
+    {
+        public string Name { get; private set; }
+        public decimal Price { get; set; }
+
+        public Product(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        Product()
+        {
+        }
+
+        public static List<Product> GetSampleList()
+        {
+            return new List<Product>()
+            {
+                new Product() { Name = "SalihP", Price = 22m },
+                new Product(name: "SabanP", price: 13m),
+                new Product() { Name = "SinanP", Price = 12m },
+                new Product(name: "SabanP", price: 155m)
+            };
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Name of product: {0}, Price of product: {1}", Name, Price);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpInDepthP1
+{
+    public class ProductNameComparer : IComparer<Product>
+    {
+        public int Compare(Product x, Product y)
+        {
+            return x.Price.CompareTo(y.Price);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> prodList = Product.GetSampleList();
+            prodList.Sort(new ProductNameComparer());
+            WriteList(prodList);
+
+            prodList.Sort(delegate (Product x, Product y) { return x.Name.CompareTo(y.Name); });
+            WriteList(prodList);
+        }
+
+        static void WriteList(List<Product> list)
+        {
+            foreach (Product item in list)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            Console.WriteLine("===================================================\n");
+        }
+    }
+}
